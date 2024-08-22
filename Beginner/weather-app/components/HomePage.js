71855f8e5a5f8798b675b6cef28de1266b025b48
0122345 <ntwarifiacre043@gmail.com>
2024-08-22 18:45:14 +0200

    removin unwanted assets

--- Beginner/weather-app/components/HomePage.js
+++ Beginner/weather-app/components/HomePage.js
@@ -1,9 +1,26 @@
 import React from 'react'
+import Image from 'next/image'
 
 const HomePage = () => {
   return (
-    <main className='w-dvw bg-slate-300 h-dvh'>
-      <p className='text-4xl text-black '> Yoo </p>
+    <main className='w-dvw h-dvh overflow-hidden'>
+      <Image
+      src="/assets/rainny/rain03.jpg"
+      alt="backgroundImage"
+      quality={100}
+      width={100}
+      height={100}
+      style={
+        { 
+          objectFit: 'cover', 
+          width: '100%',
+          height: '100%',
+          position: 'absolute',
+          zIndex: '10',
+        }
+      }
+    
+      />
     </main>
   )
 }
